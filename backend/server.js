721581717e1c5096c321382e5bcfe36f980551de
jemalmewjebhe <24078@supnum.mr>
2025-12-05 04:41:34 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,9 +7,13 @@ const app = express();
 app.use(cors({ origin: "*" }));
 app.use(express.json({ limit: "5mb" }));
 
-app.use(express.static("backend/public"));
+// app.use(express.static("backend/public"));
+app.use(express.static("public"));
 // API routes
 app.use("/api", analyzeRoute);
+app.use((req, res) => {
+    res.status(404).json({ error: "Route not found" });
+});
 
 // Start server
 const PORT = 5000;
